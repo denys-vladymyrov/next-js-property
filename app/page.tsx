4ebcde36f609ff1f50f39f378763c050a2ad240b
Denys Vladymyrov <link_amd@yahoo.com>
2025-02-04 18:35:43 +0200

    added FeatureProperty section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Hero from '../components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from "@/components/HomeProperties";
-import connectDb from "@/config/database";
+import FeaturedProperties from '@/components/FeaturedProperties';
 
 const HomePage = () => {
-
-  connectDb();
 
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </>
   );
